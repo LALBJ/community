6a1fa53320516df1c958a4725f8aabd563b9383d
LALBJ <905976755@qq.com>
2019-09-05 10:50:21 +0800

    实现了发布模块，并且添加了用户头像信息

---
+++ src/main/java/life/majiang/communtity/community/model/Question.java
@@ -0,0 +1,16 @@
+package life.majiang.communtity.community.model;
+import lombok.Data;
+
+@Data
+public class Question {
+    private Integer id;
+    private String title;
+    private String description;
+    private String tag;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Integer creator;
+    private Integer viewCount;
+    private Integer commentCount;
+    private Integer likeCount;
+}
--- src/main/java/life/majiang/communtity/community/model/User.java
+++ src/main/java/life/majiang/communtity/community/model/User.java
@@ -1,59 +1,14 @@
 package life.majiang.communtity.community.model;
 
+import lombok.Data;
+
+@Data
 public class User {
     private Integer id;
     private String name;
     private String accountId;
     private String token;
     private Long gmtCreate;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Long gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Long getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Long gmtModified) {
-        this.gmtModified = gmtModified;
-    }
-
+    private String avatarUrl;
     private Long gmtModified;
 }
